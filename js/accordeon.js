c108cd2d3d816cfea1f2eaf87b567b54371b5c82
diatomiccoder <chudak_93@live.com>
2018-11-21 01:39:00 +0500

    fix

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,34 +1,3 @@
-// (function () {
-//     function toggleClass(item) {
-//         return function(e) {
-//             e.preventDefault();
-//             item.classList.toggle('menu__item--active');
-//         };
-//     }
-//     let menuItems = document.querySelectorAll('.menu__list .menu__item');
-
-//     for (let item of menuItems) {
-//         let trigger = item.querySelector('.menu__item-trigger');
-//         trigger.addEventListener("click", toggleClass(item));
-//     }
-// })();
-
-// (function() {
-//     function toggleClass(item) {
-//         return function(e) {
-//             e.preventDefault();
-//             item.classList.toggle('team__member--active');
-//         };
-//     }
-
-//     let menuItems = document.querySelectorAll('.team__list .team__member');
-
-//     for (let item of menuItems) {
-//         let trigger = item.querySelector('.team__member-trigger');
-//         trigger.addEventListener("click", toggleClass(item));
-//     }
-// })();
-
 const accordeon = (function() {
     function toggleClass(item, activeClass) {
         return function(e) {
